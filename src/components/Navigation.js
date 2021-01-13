8100c2c1c84a35669c5973b97253f3757a7bd129
Miranda <mtran3@wellesley.edu>
2021-01-13 14:47:51 -0500

    add font styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,9 @@ export default class Navigation extends Component {
     render() {
         return (
             <header className='app-logo'>
-                <span></span>
-                <Link to='/'>Miranda Tran</Link>
+                <Link to='/' className='logo'>Miranda Tran</Link>
+                <Link to='/education' className='header-item'>Education</Link>
+                <Link to='/projects' className='header-item'>Projects</Link>
             </header>
         )
     }

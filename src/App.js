ef8226f43c8e340c0570a33043f042e815d843b9
Miranda <mtran3@wellesley.edu>
2021-01-22 12:53:51 -0500

    convert html navigation bar to React

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Home from './components/Home'
 import Project from './components/Projects';
 import Education from './components/Education';
 import Navigation from './components/Navigation';
+import Header from './components/Header';
 
 function App() {
   return (
     
       <BrowserRouter>
         <div className='App'>
-          <Navigation />
+          <div id='wrapper'>
 
-          <Route exact path='/' component={Home} />
-          <Route path='/projects' component={Project} />
-          <Route path='/education' component={Education} />
+            <div id='main'>
+              <div className='inner'>
+                
+                <Route exact path='/' component={Home} />
+                <Route path='/projects' component={Project} />
+                <Route path='/education' component={Education} />
+              </div>
+
+            </div>
+
+            <Header />
+
+          </div>
         </div>
       
       </BrowserRouter>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,29 +4,7 @@ import { Link, NavLink }  from 'react-router-dom';
 export default class Navigation extends Component {
     render() {
         return (
-            <section id="header">
-				<header>
-					<h1 id="logo"><NavLink exact to='/'>Miranda Tran</NavLink></h1>
-					<p>MSCS student at Northeastern, seeking co-op/internship for Summer/Fall 2021.<br />
-                    Enjoys learning about new technologies, Twitch, and making dinner rolls.</p>
-				</header>
-				<nav id="nav">
-					<ul>
-						<li><NavLink to='/projects'>Projects</NavLink></li>
-						<li><NavLink to='/education'>Education</NavLink></li>
-						<li><NavLink to='googmle.com'>Contact</NavLink></li>
-					</ul>
-				</nav>
-				<footer>
-					<ul class="icons">
-						<li><Link to='google.com' class="icon brands fa-twitter"><span class="label">Twitter</span></Link></li>
-						<li><Link to='google.com' class="icon brands fa-facebook-f"><span class="label">Facebook</span></Link></li>
-						<li><Link to='google.com' class="icon brands fa-instagram"><span class="label">Instagram</span></Link></li>
-						<li><Link to='google.com' class="icon brands fa-github"><span class="label">Github</span></Link></li>
-						<li><Link to='google.com' class="icon solid fa-envelope"><span class="label">Email</span></Link></li>
-					</ul>
-				</footer>
-			</section>
+			<div>hi</div>
         )
     }
 }

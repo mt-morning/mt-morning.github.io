87da0cc1f49915c11183ba11197d6316f5f1ea85
Miranda <mtran3@wellesley.edu>
2021-01-22 13:32:23 -0500

    add header contact links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import { Link, NavLink }  from 'react-router-dom';
+import { NavLink, Link }  from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
-			<div>hi</div>
+			<header id='header'>
+                <NavLink to='/' className='logo'><strong>Miranda</strong> Tran</NavLink>
+                <ul className='icons'>
+                    <li><a href='https://www.linkedin.com/in/mirandamytran/' className="icon brands fa-linkedin-in"><span className="label">LinkedIn</span></a></li>
+                    <li><a href='https://github.com/mt-morning' className="icon brands fa-github"><span className="label">GitHub</span></a></li>
+                    <li><a href='mailto: tran.mir@northeastern.edu' className="icon brands fa-envelope"><span className="label">Email</span></a></li>
+                </ul>
+            </header>
         )
     }
 }

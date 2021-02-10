577d2105979dafd1bbdf837a10d59e9ec70ca6b8
Miranda <mtran3@wellesley.edu>
2021-02-09 22:28:04 -0500

    add resume

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -42,6 +42,7 @@ export default class Sidebar extends Component {
                             <li><NavLink to='/'>Home</NavLink></li>
                             <li><NavLink to='/projects'>Projects</NavLink></li>
                             <li><NavLink to='/education'>Education</NavLink></li>
+                            <li><a href='https://drive.google.com/file/d/15cA5QB288-LV9JlduYUB7gg2Bh7lW5ZP/view?usp=sharing'>Resume</a></li>
                         </ul>
                         
                     </nav>
